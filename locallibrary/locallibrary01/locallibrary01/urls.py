a8d91dce4d4aadaf04c61a416fb605e1341c0916
inconstantelebasI <isabelle.constant51@aluno.ifce.edu.br>
2025-10-23 09:22:29 -0300

    rodamos o server do django para manipularmos a livraria que criamos

--- locallibrary/locallibrary01/locallibrary01/urls.py
+++ locallibrary/locallibrary01/locallibrary01/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for locallibrary01 project.
+URL configuration for locallibrary project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -16,7 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls')),
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
 ]
+
+
+# isto carrega os arquivos estáticos durante o desenvolvimento
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns += static(settings.STATIC_URL,
+document_root=settings.STATIC_ROOT)
